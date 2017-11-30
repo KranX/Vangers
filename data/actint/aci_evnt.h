20572143db65b4f4cf171ba5233d7f974ecf3e68
stalkerg <stalkerg@gmail.com>
2017-12-01 08:50:49 +0900

    Readd actint file with right end of line.

---
+++ data/actint/aci_evnt.h
@@ -0,0 +1,40 @@
+
+// Some special keys...
+#define ACS_STARTUP_KEY 	0x1001
+
+// aciScreenEventCommand::code values...
+#define ACS_SET_RESOURCE	0
+#define ACS_EXEC_SEQUENCE	1
+#define ACS_EXT_EVENT		2
+#define ACS_EXIT		3
+#define ACS_CHANGE_SCROLLER	4
+#define ACS_INPUT_STRING	5
+#define ACS_CHANGE_STR_STATE	6
+
+// External events...
+#define ACS_GLOBAL_EXIT 		0
+
+#define ACS_SET_SOUND_MODE		1
+#define ACS_SET_MUSIC_MODE		2
+#define ACS_SET_SOUND_VOLUME		3
+#define ACS_SET_MUSIC_VOLUME		4
+#define ACS_SET_TUTORIAL_MODE		5
+
+#define ACS_INIT_SOUND_MODE		6
+#define ACS_INIT_MUSIC_MODE		7
+#define ACS_INIT_SOUND_VOLUME		8
+#define ACS_INIT_MUSIC_VOLUME		9
+#define ACS_INIT_TUTORIAL_MODE		10
+
+#define ACS_CHANGE_SOUND_MODE		11
+#define ACS_CHANGE_MUSIC_MODE		12
+#define ACS_CHANGE_TUTORIAL_MODE	13
+
+#define ACS_INIT_SLOT_NAME		14
+#define ACS_INIT_SLOT_NAME_INPUT	15
+#define ACS_SAVE_GAME			16
+
+#define ACS_SET_AUTORUN_MODE		17
+#define ACS_INIT_AUTORUN_MODE		18
+#define ACS_CHANGE_AUTORUN_MODE 	19
+
