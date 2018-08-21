aa5cb1229d6f207b6311df274ea335debfb3075e
nikitap <lpenguin@yandex.ru>
2018-08-21 14:57:31 +0300

    small fixes

--- src/units/mechos.cpp
+++ src/units/mechos.cpp
@@ -3311,7 +3311,10 @@ void camera_quant(int X, int Y, int Turn, double V_abs) {
 	                                                     (-SLOPE_MAX - camera_slope_min) * V_abs / camera_vmax :
 	                               -SLOPE_MAX)
 	                            : camera_slope_min;
-	s = -SLOPE_MAX;
+	if(camera_slope_enable){
+		s = -SLOPE_MAX;	
+	}	                            
+	
 	camera_vs += (double) (s - SlopeAngle) * camera_mis * XTCORE_FRAME_NORMAL;
 	camera_vs *= camera_drags * pow(0.97, camera_vs_min / (fabs(camera_vs) + 1e-10));
 	camera_s += camera_vs * XTCORE_FRAME_NORMAL;
