08a74b58fc69560423ab1e5c1465570a339f0c98
Nikita Prianichnikov <naprianichnikov@peraltech.ru>
2019-01-05 12:53:22 +0300

    Disabled selecting objects on map with mouse

--- src/units/hobj.cpp
+++ src/units/hobj.cpp
@@ -1313,55 +1313,52 @@ void GameObjectDispatcher::DrawQuant(void) {
 	}
 	CreateTabutaskTarget();
 	CompasObj.Quant();
-	if(XGR_MouseObj.lBt.Pressed){
-		auto globalX = ViewX + XGR_MouseObj.PosX - ScreenCX;
-		auto globalY = ViewY + XGR_MouseObj.PosY - ScreenCY;
-
-		std::cout<<"Mouse: "<<XGR_MouseObj.PosX<<" "<<XGR_MouseObj.PosY<<" "<<XGR_MouseObj.PosZ<<std::endl;
-		std::cout<<"Global coords: "<<globalX<<" "<<globalY<<std::endl;
-
-		selected = nullptr;
-		BaseObject* minSelected = nullptr;
-		float minDist = 99999999;
-		auto* t = Tail;
-		while(t){
-
-			auto dx = globalX - t->R_curr.x;
-			auto dy = globalY - t->R_curr.y;
-			auto dist = sqrt(dx * dx + dy * dy);
-
-			if(dist < t->radius){
-				if(minDist > dist){
-					minDist = dist;
-					minSelected = t;
-				}
-			}
-
-			t = t->NextBaseList;
-		}
-		if(minSelected != nullptr){
-			selected = minSelected;
-		}
-	}
-
-	if(selected != nullptr){
-		int center_x, center_y;
-		global_to_screen_coords(selected->R_curr.x, selected->R_curr.y, selected->R_curr.z, center_x, center_y);
-		char buf[256];
-		sprintf(buf, "x: %d, y: %d:, z: %d, radius: %d, id: %d",
-				selected->R_curr.x, selected->R_curr.y, selected->R_curr.z,
-				selected->radius, selected->ID
-				);
-		XGR_OutText(center_x + 10, center_y, 253, buf, XGR_FONT0_8x16);
-		for(int tx = center_x - 10; tx < center_x + 10; tx++){
-			for(int ty = center_y - 10; ty < center_y + 10; ty++){
-				if(tx > UcutLeft && tx < UcutRight && ty > VcutUp && ty < VcutDown){
-					XGR_SetPixelFast(tx, ty, 253);
-				}
-			}
-		}
-
-	}
+	// TODO: Selecting objects is unstable
+//	if(XGR_MouseObj.lBt.Pressed){
+//		int globalX, globalY;
+//		S2G(XGR_MouseObj.PosX, XGR_MouseObj.PosY, globalX, globalY);
+//
+//		selected = nullptr;
+//		BaseObject* minSelected = nullptr;
+//		float minDist = 99999999;
+//		auto* t = Tail;
+//		while(t){
+//			auto dx = getDistX(globalX, t->R_curr.x);
+//			auto dy = getDistY(globalY, t->R_curr.y);
+//			auto dist = sqrt(dx * dx + dy * dy);
+//
+//			if(dist < max(t->radius, 15)){
+//				if(minDist > dist){
+//					minDist = dist;
+//					minSelected = t;
+//				}
+//			}
+//
+//			t = t->NextBaseList;
+//		}
+//		if(minSelected != nullptr){
+//			selected = minSelected;
+//		}
+//	}
+//
+//	if(selected != nullptr){
+//		int center_x, center_y;
+//		global_to_screen_coords(selected->R_curr.x, selected->R_curr.y, selected->R_curr.z, center_x, center_y);
+//		char buf[256];
+//		sprintf(buf, "x: %d, y: %d:, z: %d, radius: %d, id: %d",
+//				selected->R_curr.x, selected->R_curr.y, selected->R_curr.z,
+//				selected->radius, selected->ID
+//				);
+//		XGR_OutText(center_x + 10, center_y, 253, buf, XGR_FONT0_8x16);
+//		for(int tx = center_x - 10; tx < center_x + 10; tx++){
+//			for(int ty = center_y - 10; ty < center_y + 10; ty++){
+//				if(tx > UcutLeft && tx < UcutRight && ty > VcutUp && ty < VcutDown){
+//					XGR_SetPixelFast(tx, ty, 253);
+//				}
+//			}
+//		}
+//
+//	}
 };
 
 void setMapPixel(int px,int py,int col)
