ebbc92a3d623f42313e23d114e20b48ffb2e8ff2
Vasily Postnicov <shamaz.mazum@gmail.com>
2020-04-23 12:34:54 +0300

    Avoid dangling elses

--- src/sound/sound.cpp
+++ src/sound/sound.cpp
@@ -444,40 +444,45 @@ void SoundQuant(void)
 	int DiffSound = SoundFlag ^ lastSoundFlag;
 	
 	if (DiffSound){
-		if (DiffSound & SoundCopterig)
-			if (SoundFlag & SoundCopterig){
+		if (DiffSound & SoundCopterig) {
+			if (SoundFlag & SoundCopterig) {
 				SOUND_COPTERIG_START()
-			}else{
+			} else {
 				SOUND_COPTERIG_STOP()
 			}
+		}
 
-		if (DiffSound & SoundCrotrig)
-			if (SoundFlag & SoundCrotrig){
+		if (DiffSound & SoundCrotrig) {
+			if (SoundFlag & SoundCrotrig) {
 				SOUND_CROTRIG_START()
-			}else {
+			} else {
 				SOUND_CROTRIG_STOP()
 			}
+		}
 
-		if (DiffSound & SoundCutterig)
-			if (SoundFlag & SoundCutterig){
+		if (DiffSound & SoundCutterig) {
+			if (SoundFlag & SoundCutterig) {
 				SOUND_CUTTERIG_START()
-			}else{
+			} else {
 				SOUND_CUTTERIG_STOP()
 			}
+		}
 
-		if (DiffSound & SoundUnderWater)
-			if (SoundFlag & SoundUnderWater){
+		if (DiffSound & SoundUnderWater) {
+			if (SoundFlag & SoundUnderWater) {
 				SOUND_UNDERWATER_START()
-			}else{
+			} else {
 				SOUND_UNDERWATER_STOP()
 			}
+		}
 
-		if (DiffSound & SoundMotor)
-			if (SoundFlag & SoundMotor){
+		if (DiffSound & SoundMotor) {
+			if (SoundFlag & SoundMotor) {
 				SOUND_START_MOTOR()
-			}else {
+			} else {
 				SOUND_STOP_MOTOR()
 			}
+		}
 	}
 		
 	lastSoundFlag = SoundFlag;
