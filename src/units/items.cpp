9253e15838d0850fb455f510fe689ab402279f3d
Ілля Махнотов <70032794+Tstis@users.noreply.github.com>
2020-10-13 14:02:41 +0300

    Update items.cpp

--- src/units/items.cpp
+++ src/units/items.cpp
@@ -3883,15 +3883,31 @@ void GloryPlace::Init(int ind)
 
 			while (World == WORLD_HMOK)
 				World = GloryRnd.aiRnd(WORLD_MAX);
-			};
+			}
 		R_curr.x = GloryRnd.aiRnd(WorldTable[World]->x_size);
-			if(World < MAIN_WORLD_MAX - 1)
-				R_curr.y = 300 + GloryRnd.aiRnd(WorldTable[World]->y_size - 600);
-			else	
-				R_curr.y = GloryRnd.aiRnd(WorldTable[World]->y_size);
-			return;
+		if(World < MAIN_WORLD_MAX - 1)
+			R_curr.y = 300 + GloryRnd.aiRnd(WorldTable[World]->y_size - 600);
+		else	
+			R_curr.y = GloryRnd.aiRnd(WorldTable[World]->y_size);
+		return;
+	}
+	
+	//test-h
+	if(NetworkON && my_server_data.GameType == PASSEMBLOSS && strcmp(iScrOpt[iSERVER_NAME]->GetValueCHR(), "2") == 0) {
+		if(ind == 0) {
+			R_curr.x =  150;	R_curr.y = 5435; World = WORLD_GLORX;
+		} else {
+			World = GloryRnd.aiRnd(WORLD_MAX);
+			
+			while (World == WORLD_HMOK)
+				World = GloryRnd.aiRnd(WORLD_MAX);
+			}
+			R_curr.y = 300 + GloryRnd.aiRnd(WorldTable[World]->y_size - 600);
+			R_curr.y = GloryRnd.aiRnd(WorldTable[World]->y_size);
 		}
-		
+		return;
+	}
+	
 	//aveslom
 	if (NetworkON && my_server_data.GameType == PASSEMBLOSS && strcmp(iScrOpt[iSERVER_NAME]->GetValueCHR(), "aveslom")==0) {
 		if(ind == 0) {
