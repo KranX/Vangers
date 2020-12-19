192a6ba9f53fd83d160ff5a64b8d30a7bc9b61af
Kiifts thi Iftusihtust <aid@kiv.name>
2020-12-20 02:16:27 +0400

    Fixed incorrect shadow drawing + crashes due to the same shadows in certain circumstances (with rotated camera)

--- src/3d/optimize.cpp
+++ src/3d/optimize.cpp
@@ -2169,7 +2169,7 @@ void CastArbitraryShadow(int x,int y,int zg,int size,int shift,unsigned char* bu
 	x0 = SHL16(X1) - x_bmp_corner_16;
 	y0 = (Y0 << 16) - y_bmp_corner_16;
 	dx0 = 0;
-	dy0 = dy <= 0 ? (1 << 16) : (-1 & 0xFFFF);
+	dy0 = dy <= 0 ? 65536 : -65536;
 	bmp_len = 0;
 	d_bmp_len = d_1_y;
 
