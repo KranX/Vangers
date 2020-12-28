652a1632226feac44f4505dae37e8671e5956cb9
XiadaOku <egor.poskalov@gmail.com>
2020-12-28 18:19:10 +0500

    input bug fix

--- src/iscreen/iscreen.cpp
+++ src/iscreen/iscreen.cpp
@@ -3407,6 +3407,7 @@ void iScreenDispatcher::init_input_string(iScreenElement* p)
 
 void iScreenDispatcher::input_string_quant(void)
 {
+	SDL_StartTextInput();
 	int sz,init_flag = 0,redraw_flag = 0;
 	iListElementPtr* tmp;
 	iScreenObject* obj;
@@ -3445,6 +3446,7 @@ void iScreenDispatcher::input_string_quant(void)
 					flags |= SD_FINISH_INPUT;
 					init_flag = 1;
 					redraw_flag = 1;
+					SDL_StopTextInput();
 					break;
 				case SDLK_ESCAPE:
 					strcpy((char*)ptr,BackupStr);
@@ -3452,6 +3454,7 @@ void iScreenDispatcher::input_string_quant(void)
 					flags |= SD_FINISH_INPUT;
 					init_flag = 1;
 					redraw_flag = 1;
+					SDL_StopTextInput();
 					break;
 				case SDLK_LEFT:
 				case SDLK_BACKSPACE:
