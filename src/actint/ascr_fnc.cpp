1a7b97cda76f0d5c47af0564db1b73b07351c7e5
Ілля Махнотов <70032794+Tstis@users.noreply.github.com>
2021-02-07 19:30:29 +0300

    Update ascr_fnc.cpp

--- src/actint/ascr_fnc.cpp
+++ src/actint/ascr_fnc.cpp
@@ -5752,7 +5752,7 @@ void aciShowFrags(void)
 				p = iPlayers[i];
 				XBuf.init();
 				world_name = aScrDisp -> wMap -> world_ptr[aScrDisp -> wMap -> world_ids[p -> body.world]] -> name;
-				XBuf < p -> name < " (" < world_name < ") : " < aciSTR_Ware1 < " " <= p -> body.MechosomaStat.ItemCount1 < "/" <= my_server_data.Mechosoma.ProductQuantity1;
+				XBuf < p -> name < " : " < aciSTR_Ware1 < " " <= p -> body.MechosomaStat.ItemCount1 < "/" <= my_server_data.Mechosoma.ProductQuantity1;
 				XBuf < " " < aciSTR_Ware2 < " " <= p -> body.MechosomaStat.ItemCount2 < "/" <= my_server_data.Mechosoma.ProductQuantity2 < " | " <= p -> body.kills < " " < aciSTR_KILLS < " " <= p -> body.deaths < " " < aciSTR_DEATHS;
 				aScrDisp -> curPrompt -> add_str(i,(unsigned char*)XBuf.address());
 				aScrDisp -> curPrompt -> TimeBuf[i] = ACI_FRAG_TIMER;
