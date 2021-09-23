b802a5f1a95017c0ef6b618f5f8fd4c58195ba29
EXL <exlmotodev@gmail.com>
2021-09-23 18:45:05 +0700

    Fix function without return statement

--- surmap/missed.cpp
+++ surmap/missed.cpp
@@ -111,7 +111,7 @@ char* win32_findnext() {
 }
 
 // we should still rely on gcc 5
-std::string replace_all(std::string& str, const std::string& from, const std::string& to) {
+void replace_all(std::string& str, const std::string& from, const std::string& to) {
     size_t start_pos = 0;
     while((start_pos = str.find(from, start_pos)) != std::string::npos) {
         str.replace(start_pos, from.length(), to);
