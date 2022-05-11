4ae80623d1b9664e10b31bd3cff745319ead03e6
dilesoft <dilesoft@gmail.com>
2022-05-11 12:47:25 +0300

    indent change

--- lib/xtool/xerrhead.cpp
+++ lib/xtool/xerrhead.cpp
@@ -47,35 +47,42 @@ void XErrorHandler::RTC(const char *file, unsigned int line, const char *expr)
 
 void XErrorHandler::Abort(const char* message, int code, int val, const char* subj)
 {
-	std::ostringstream stream;
-	time_t now;
-    time(&now);
-    char time_buf[sizeof "2011-10-08T07:07:09Z"];
-    strftime(time_buf, sizeof time_buf, "%Y-%m-%dT%H:%M:%SZ", gmtime(&now));
+  std::ostringstream stream;
+  
+  time_t now;
+  time(&now);
+  char time_buf[sizeof "2011-10-08T07:07:09Z"];
+  strftime(time_buf, sizeof time_buf, "%Y-%m-%dT%H:%M:%SZ", gmtime(&now));
+  stream<<time_buf<<" "<<std::endl;
 
-	stream<<time_buf<<" "<<std::endl;
-		stream<<GIT_BRANCH<<GITHUB_BRANCH<<" "<<GIT_COMMIT_HASH<<GITHUB_COMMIT_HASH<<std::endl<<std::endl;
+  stream<<GIT_BRANCH<<GITHUB_BRANCH<<" "<<GIT_COMMIT_HASH<<GITHUB_COMMIT_HASH<<std::endl<<std::endl;
 
-stream<<"Abort: "<<message<<" code:"<<code<<" val:"<<val<<std::endl;
-	std::cout<<"Abort: "<<message<<" code:"<<code<<" val:"<<val<<std::endl;
-	if (subj)
-		stream<<"Subj:"<<subj<<std::endl<<std::endl;
-	stream<<"Please send:" << std::endl <<
-	" - this message," << std::endl <<
-	" - logfile from " << SDL_GetBasePath() << log_name.c_str() << "," << std::endl <<
-	" - your savegame" << std::endl <<
-	"to https://t.me/vangers or https://github.com/KranX/Vangers";
-	std::string str =  stream.str();
+  stream<<"Abort: "<<message<<" code:"<<code<<" val:"<<val<<std::endl;
+  std::cout<<"Abort: "<<message<<" code:"<<code<<" val:"<<val<<std::endl;
+
+  if (subj)
+    stream<<"Subj:"<<subj<<std::endl<<std::endl;
+
+  stream<<"Please send:" << std::endl <<
+    " - this message," << std::endl <<
+    " - logfile from " << SDL_GetBasePath() << log_name.c_str() << "," << std::endl <<
+    " - your savegame" << std::endl <<
+    "to https://t.me/vangers or https://github.com/KranX/Vangers";
+
+  std::string str =  stream.str();
   std::cout<<str;
-	log_file.open(log_name.c_str(),std::ios::out|std::ios::app);
+
+  log_file.open(log_name.c_str(),std::ios::out|std::ios::app);
   log_file<<str;
-	log_file.close();
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
-		"Vangers error",
-		str.c_str(),
-		NULL);
-	//MessageBox(NULL,outmsg,prefix,attr | MB_TOPMOST | MB_SYSTEMMODAL);
-	exit(code);
+  log_file.close();
+
+  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,
+    "Vangers error",
+    str.c_str(),
+    NULL
+  );
+  //MessageBox(NULL,outmsg,prefix,attr | MB_TOPMOST | MB_SYSTEMMODAL);
+  exit(code);
 }
 
 void XErrorHandler::Log(const char* message)
