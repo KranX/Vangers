2abceb16730afeb3cafd987d74a0c1dd85c7f76e
Alexander Guryanov <caiiiycuk@gmail.com>
2022-07-06 12:20:20 +0700

    Do not affect actual mouse position with spot point

--- lib/xgraph/xgraph.cpp
+++ lib/xgraph/xgraph.cpp
@@ -1656,8 +1656,8 @@ static unsigned short XGR_MouseDefFrameHC[240] =
 
 void XGR_Mouse::Init(int x,int y,int sx,int sy,int num,void* p)
 {
-	PosX = x;
-	PosY = y;
+	PosX = x - SpotX;
+	PosY = y - SpotX;
 	SizeX = sx;
 	SizeY = sy;
 	PosZ = LastPosZ = MovementZ = 0;
@@ -1845,8 +1845,8 @@ void XGR_Mouse::InitPos(int x,int y)
 	LastSizeX = SizeX;
 	LastSizeY = SizeY;
 
-	PosX = x;
-	PosY = y;
+	PosX = x - SpotX;
+	PosY = y - SpotY;
 
 	AdjustPos();
 }
@@ -1881,10 +1881,10 @@ void XGR_Mouse::SetPos(int x,int y)
 	LastSizeX = SizeX;
 	LastSizeY = SizeY;
 
-	PosX = x;
-	PosY = y;
+	PosX = x - SpotX;
+	PosY = y - SpotY;
 
-	//AdjustPos();
+	AdjustPos();
 
 //	pt.x = PosX;
 //	pt.y = PosY;
