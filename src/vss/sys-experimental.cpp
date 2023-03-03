6d404413c9dce256c371220be52d2253a4b4e08e
Alexander Guryanov <aguryanov@think-cell.com>
2023-03-03 09:26:18 +0100

    add vss_experimental impl for native build

---
+++ src/vss/sys-experimental.cpp
@@ -0,0 +1,31 @@
+//
+// Created by aguryanov on 3/3/2023.
+//
+
+#ifndef ANDROID
+
+bool vss_experimental_menuChangeRequested() {
+  return false;
+}
+
+bool vss_experimental_menuUpRequested() {
+  return false;
+}
+
+bool vss_experimental_menuDownRequested() {
+  return false;
+}
+
+bool vss_experimental_menuResetRequest() {
+  return false;
+}
+
+bool vss_experimental_menuWasChanged() {
+  return false;
+}
+
+bool vss_experimental_menuActivateRequested() {
+  return false;
+}
+
+#endif
